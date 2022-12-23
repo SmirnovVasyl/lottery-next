08fae42c4b1656567b409a78dea947da61fe9bd6
yuka0705 <smirnovvvasyl@gmail.com>
2022-12-23 06:47:34 -0800

    first commit

---
+++ components/header/header.config.js
@@ -0,0 +1,8 @@
+export const menuList = [
+  { title: 'Home', link: 'home' },
+  { title: 'About', link: 'about' },
+  { title: 'Roadmap', link: 'loadmap' },
+  { title: 'lottery', link: 'lottery' },
+  { title: 'Statistic', link: 'statistic' },
+  { title: 'Dashboard', link: 'dashboard' },
+]
